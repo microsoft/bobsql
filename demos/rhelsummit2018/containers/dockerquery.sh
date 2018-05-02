85b4462b8c68b5a09ab6c9614cd247fa6cea7839
Bob Ward <bobward@microsoft.com>
2018-05-02 10:53:16 -0500

    Fixed dockerquery.sh

--- demos/rhelsummit2018/containers/dockerquery.sh
+++ demos/rhelsummit2018/containers/dockerquery.sh
@@ -1,1 +1,1 @@
-sqlcmd -Usa -s127.0.0.1,1401 -Q'USE WideWorlImporters;SELECT * FROM [Application].[People];'
+sqlcmd -Usa -S127.0.0.1,1401 -Q'USE WideWorldImporters;SELECT * FROM [Application].[People];'
