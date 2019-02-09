c4f0940f7fccbbddb5a04c7546fa647a45b1f9ac
Bob Ward <bobward@microsoft.com>
2019-02-09 16:21:08 -0600

    added hostname to docker run script for upgrade

--- demos/containers/sqlcontainerupdate/step6_dockerupgrade.sh
+++ demos/containers/sqlcontainerupdate/step6_dockerupgrade.sh
@@ -1,3 +1,3 @@
 sudo docker stop sql2017cu10
-sudo docker run -e 'ACCEPT_EULA=Y' -e 'MSSQL_SA_PASSWORD=Sql2017isfast' -p 1401:1433 -v sqlvolume:/var/opt/mssql --name sql2019 -d mcr.microsoft.com/mssql/rhel/server:vNext-CTP2.0
+sudo docker run -e 'ACCEPT_EULA=Y' -e 'MSSQL_SA_PASSWORD=Sql2017isfast' -p 1401:1433 -v sqlvolume:/var/opt/mssql --hostname sql2019 --name sql2019 -d mcr.microsoft.com/mssql/rhel/server:vNext-CTP2.0
 
