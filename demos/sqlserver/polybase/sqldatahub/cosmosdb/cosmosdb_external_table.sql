f2670864f4072cc516102b3e2c5cf52c79da28ac
Bob Ward <bobward@microsoft.com>
2019-02-10 17:15:06 -0600

    posted cosmosdb demo

--- demos/sqlserver/polybase/sqldatahub/cosmosdb/cosmosdb_external_table.sql
+++ demos/sqlserver/polybase/sqldatahub/cosmosdb/cosmosdb_external_table.sql
@@ -8,15 +8,14 @@ GO
 */
 DROP DATABASE SCOPED CREDENTIAL CosmosDBCredentials
 GO
+-- You can get the IDENTITY (user) and secret (password) from the Connection String option in the
+-- Azure portal
 CREATE DATABASE SCOPED CREDENTIAL CosmosDBCredentials   
 WITH IDENTITY = 'wwi', Secret = 'hSoxMUeEgNjeeWh4FTz5jmGRlSN4Ko6HoYqiJsbleFzewe86EEXJrvwkAqBgitypJdjUbeJqnTVNBO6NUa0DZQ=='
 GO
-/*  LOCATION: Location string should be of format '<vendor>://<server>[:<port>]'.
-*  PUSHDOWN: specify whether computation should be pushed down to the source. ON by default.
-*  CREDENTIAL: the database scoped credential, created above.
-*/  
 DROP EXTERNAL DATA SOURCE CosmosDB
 GO
+-- The LOCATION is built from <HOST>:<PORT> from the Connection String in the Azure Portal
 CREATE EXTERNAL DATA SOURCE CosmosDB
 WITH ( 
 LOCATION = 'mongodb://wwi.documents.azure.com:10255',
@@ -35,20 +34,36 @@ DROP EXTERNAL TABLE cosmosdb.Orders
 GO
 CREATE EXTERNAL TABLE cosmosdb.Orders
 (
-	[_id] NVARCHAR(100) NOT NULL,
-	[id] NVARCHAR(100) NOT NULL,
-	[OrderID] NVARCHAR(100) NOT NULL,
-	[CustomerName] NVARCHAR(100) NOT NULL,
-	[CustomerContact] NVARCHAR(100) NOT NULL,
-	[OrderDate] NVARCHAR(100) NOT NULL,
-	[CustomerPO] NVARCHAR(100) NULL,
-	[ExpectedDeliverDate] NVARCHAR(100) NOT NULL
+	[_id] NVARCHAR(100) COLLATE Latin1_General_100_CI_AS NOT NULL,
+	[id] NVARCHAR(100) COLLATE Latin1_General_100_CI_AS NOT NULL,
+	[OrderID] int NOT NULL,
+	[SalesPersonPersonID] int NOT NULL,
+	[CustomerName] NVARCHAR(100) COLLATE Latin1_General_100_CI_AS NOT NULL,
+	[CustomerContact] NVARCHAR(100) COLLATE Latin1_General_100_CI_AS NOT NULL,
+	[OrderDate] NVARCHAR(100) COLLATE Latin1_General_100_CI_AS NOT NULL,
+	[CustomerPO] NVARCHAR(100) COLLATE Latin1_General_100_CI_AS NULL,
+	[ExpectedDeliverDate] NVARCHAR(100) COLLATE Latin1_General_100_CI_AS NOT NULL
 )
  WITH (
  LOCATION='WideWorldImporters.Orders',
  DATA_SOURCE=CosmosDB
 )
 GO
-CREATE STATISTICS CosmosDBOrderDateStats ON cosmosdb.Orders ([OrderDate]) WITH FULLSCAN
+CREATE STATISTICS CosmosDBOrderSalesPersonStats ON cosmosdb.Orders ([SalesPersonPersonID]) WITH FULLSCAN
 GO
 
+-- Scan the external table just to make sure it works
+--
+SELECT * FROM cosmosdb.Orders
+GO
+-- Filter on a specific SalesPersonPersonID
+--
+SELECT * FROM cosmosdb.Orders WHERE SalesPersonPersonID = 2
+GO
+-- Find out the name of the salesperson and which customer they worked with
+-- to test out the new mobile app experience.
+SELECT FullName, o.CustomerName, o.CustomerContact, o.OrderDate
+FROM cosmosdb.Orders o
+JOIN [Application].[People] p
+ON o.SalesPersonPersonID = p.PersonID
+GO
