73df9bedeeafa209aeb9605899636e65b1954cad
Bob Ward <bobward@microsoft.com>
2022-06-05 12:43:36 -0500

    PSP demo fixes

--- demos/sqlserver2022/IQP/psp/sqlsetup.cmd
+++ demos/sqlserver2022/IQP/psp/sqlsetup.cmd
@@ -1,1 +1,1 @@
-.\ostress -E -Q"alter database current set compatibility_level = 150; alter database current set query_store clear;DBCC TRACEON (11091, 12619, -1);" -dWideWorldImporters -q
+.\ostress -E -Q"alter database current set compatibility_level = 150; alter database current set query_store clear;" -dWideWorldImporters -q
