41208c063690e4d6fda4d4ccb9f79fbbaf6d9942
Bob Ward <bobward@microsoft.com>
2022-07-22 10:10:16 -0500

    Updates for DOP feedback for CTP 2.1

---
+++ demos/sqlserver2022/IQP/dopfeedback/check_query_feedback.sql
@@ -0,0 +1,4 @@
+USE WideWorldImporters;
+GO
+SELECT * from sys.query_store_plan_feedback;
+GO
--- demos/sqlserver2022/IQP/dopfeedback/dop_query_stats.sql
+++ demos/sqlserver2022/IQP/dopfeedback/dop_query_stats.sql
@@ -1,5 +1,8 @@
+USE WideWorldImporters;
+GO
+-- The hash value of 4128150668158729174 should be fixed for the plan from the workload
 SELECT qsp.query_plan_hash, avg_duration/1000 as avg_duration_ms, 
-avg_cpu_time/1000 as avg_cpu_ms, last_dop, min_dop, max_dop 
+avg_cpu_time/1000 as avg_cpu_ms, last_dop, min_dop, max_dop, qsrs.count_executions
 FROM sys.query_store_runtime_stats qsrs
 JOIN sys.query_store_plan qsp
 ON qsrs.plan_id = qsp.plan_id
