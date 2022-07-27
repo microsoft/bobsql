2d8fd6f6c0a089cb174fc32b0216b151ab5c23dd
Bob Ward <bobward@microsoft.com>
2022-07-26 21:42:19 -0500

    minor script change

--- demos/sqlserver2022/IQP/cefeedback/create_xevent_session.sql
+++ demos/sqlserver2022/IQP/cefeedback/create_xevent_session.sql
@@ -6,7 +6,7 @@ ADD EVENT sqlserver.query_feedback_analysis(
     ACTION(sqlserver.query_hash_signed,sqlserver.query_plan_hash_signed,sqlserver.sql_text)),
 ADD EVENT sqlserver.query_feedback_validation(
     ACTION(sqlserver.query_hash_signed,sqlserver.query_plan_hash_signed,sqlserver.sql_text))
-WITH (MAX_MEMORY=4096 KB,EVENT_RETENTION_MODE=NO_EVENT_LOSS,MAX_DISPATCH_LATENCY=1 SECONDS,MAX_EVENT_SIZE=0 KB,MEMORY_PARTITION_MODE=NONE,TRACK_CAUSALITY=OFF,STARTUP_STATE=OFF)
+WITH (MAX_MEMORY=4096 KB,EVENT_RETENTION_MODE=NO_EVENT_LOSS,MAX_DISPATCH_LATENCY=1 SECONDS,MAX_EVENT_SIZE=0 KB,MEMORY_PARTITION_MODE=NONE,TRACK_CAUSALITY=OFF,STARTUP_STATE=OFF);
 GO
 -- Start XE
 ALTER EVENT SESSION [CEFeedback] ON SERVER
