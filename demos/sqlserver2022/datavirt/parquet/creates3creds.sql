afff4f931220f6558b17adf82c0f9696c568d0a2
Bob Ward <bobward@microsoft.com>
2022-08-03 22:52:06 -0500

    Update parquet datavirt script and add notebook

--- demos/sqlserver2022/datavirt/parquet/creates3creds.sql
+++ demos/sqlserver2022/datavirt/parquet/creates3creds.sql
@@ -1,5 +1,7 @@
 USE [WideWorldImporters];
 GO
+IF EXISTS (SELECT * FROM sys.external_data_sources WHERE name = 's3_wwi')
+	DROP EXTERNAL DATA SOURCE s3_wwi;
 IF EXISTS (SELECT * FROM sys.database_scoped_credentials WHERE name = 's3_wwi_cred')
     DROP DATABASE SCOPED CREDENTIAL s3_wwi_cred;
 GO
