b71fddfb6894920903bc0116edbd053523cdab41
Bob Ward <bobward@microsoft.com>
2022-08-07 08:55:55 -0500

    Update restoretempdbfiles.sql

--- demos/sqlserver2022/tempdb/restoretempdbfiles.sql
+++ demos/sqlserver2022/tempdb/restoretempdbfiles.sql
@@ -1,6 +1,10 @@
-name	physical_name	size_kb	growth_kb
-tempdev	C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\DATA\tempdb.mdf	8192	65536
-templog	C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\DATA\templog.ldf	8192	65536
-temp2	C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\DATA\tempdb_mssql_2.ndf	8192	65536
-temp3	C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\DATA\tempdb_mssql_3.ndf	8192	65536
-temp4	C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\DATA\tempdb_mssql_4.ndf	8192	65536
+USE master;
+GO
+ALTER DATABASE tempdb MODIFY FILE (NAME=templog , SIZE = 8192Kb);
+GO
+ALTER DATABASE tempdb MODIFY FILE (NAME=temp2 , FILENAME = 'C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\DATA\empdb_mssql_2.ndf', SIZE = 8192Kb, GROWTH = 65536Kb);
+GO
+ALTER DATABASE tempdb MODIFY FILE (NAME=temp3 , FILENAME = 'C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\DATA\empdb_mssql_3.ndf', SIZE = 8192Kb, GROWTH = 65536Kb);
+GO
+ALTER DATABASE tempdb MODIFY FILE (NAME=temp4 , FILENAME = 'C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\DATA\empdb_mssql_4.ndf', SIZE = 8192Kb, GROWTH = 65536Kb);
+GO
