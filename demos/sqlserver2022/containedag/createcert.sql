f655199f0259cf1ac0c448259961f127c221345b
Bob Ward <bobward@microsoft.com>
2022-08-12 18:03:57 -0500

    contained AG updates

--- demos/sqlserver2022/containedag/createcert.sql
+++ demos/sqlserver2022/containedag/createcert.sql
@@ -1,8 +1,6 @@
 USE master;
 GO
-CREATE MASTER KEY ENCRYPTION BY PASSWORD = '<strong pwd>';
-GO
-DROP CERTIFICATE dbm_certificate;
+CREATE MASTER KEY ENCRYPTION BY PASSWORD = '$Strongpassw0rd';
 GO
 CREATE CERTIFICATE dbm_certificate WITH SUBJECT = 'dbm';
 GO
@@ -10,5 +8,5 @@ BACKUP CERTIFICATE dbm_certificate
 TO FILE = 'C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\DATA\dbm_certificate.cer'
 WITH PRIVATE KEY (
 FILE = 'c:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\DATA\dbm_certificate.pvk',
-ENCRYPTION BY PASSWORD = '<strong pwd>');
+ENCRYPTION BY PASSWORD = '$Strongpassw0rd');
 GO
--- demos/sqlserver2022/containedag/importcert.sql
+++ demos/sqlserver2022/containedag/importcert.sql
@@ -1,13 +1,11 @@
 USE master;
 GO
-CREATE MASTER KEY ENCRYPTION BY PASSWORD = '<strong pwd>';
-GO
-DROP CERTIFICATE dbm_certificate;
+CREATE MASTER KEY ENCRYPTION BY PASSWORD = '$Strongpassw0rd';
 GO
 CREATE CERTIFICATE dbm_certificate
     AUTHORIZATION dbm_user
     FROM FILE = 'C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\DATA\dbm_certificate.cer'
     WITH PRIVATE KEY (
     FILE = 'c:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\DATA\dbm_certificate.pvk',
-    DECRYPTION BY PASSWORD = '<strong pwd>');
+    DECRYPTION BY PASSWORD = '$Strongpassw0rd');
 GO
