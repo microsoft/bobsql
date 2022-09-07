8a63cf0bd52dbd2cde534120c78648a8ec7ad5a8
Bob Ward <bobward@microsoft.com>
2022-09-07 09:43:32 -0500

    Update query_plan_seek.sql

--- demos/sqlserver2022/IQP/pspopt/query_plan_seek.sql
+++ demos/sqlserver2022/IQP/pspopt/query_plan_seek.sql
@@ -2,8 +2,6 @@ USE WideWorldImporters;
 GO
 SET STATISTICS TIME ON;
 GO
-USE WideWorldImporters;
-GO
 -- The best plan for this parameter is an index seek
 EXEC Warehouse.GetStockItemsbySupplier 2;
 GO
