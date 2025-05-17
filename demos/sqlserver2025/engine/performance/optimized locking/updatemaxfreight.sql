befd26184bd74ff852456f0d6465dac84bfa7aed
Bob Ward <bobward@microsoft.com>
2025-05-17 17:54:39 -0500

    Upload OL demo

---
+++ demos/sqlserver2025/engine/performance/optimized locking/updatemaxfreight.sql
@@ -0,0 +1,13 @@
+USE AdventureWorks;
+GO
+-- Update the highest salesorderid
+DECLARE @maxsalesorderid INT;
+SELECT @maxsalesorderid = MAX(SalesOrderID) FROM Sales.SalesOrderHeader;
+BEGIN TRAN
+UPDATE Sales.SalesOrderHeader
+SET Freight = Freight * .10
+WHERE SalesOrderID = @maxsalesorderid;
+GO
+
+-- Rollback the transaction when needed
+ROLLBACK TRAN
