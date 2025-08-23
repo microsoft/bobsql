27111e6a029625107209bc4a5f0668bd5c2d9070
Bob Ward <bobward@microsoft.com>
2025-08-23 08:23:32 -0500

    Delete demos/azurepostgres directory

--- demos/azurepostgres/gettopqueries.sql
+++
@@ -1,8 +0,0 @@
-SELECT pgd.datname, qsv.query_id, qsv.total_time, qsv.blk_read_time, qsv.blk_write_time, qsv.calls, qsv.query_type, qsv.query_sql_text
-FROM query_store.qs_view qsv
-JOIN pg_database pgd
-ON qsv.db_id = pgd.oid
-WHERE pgd.datname = 'pgbench'
-AND qsv.calls > 1
-AND qsv.query_type != 'nothing'
-ORDER BY qsv.total_time/qsv.calls DESC;
