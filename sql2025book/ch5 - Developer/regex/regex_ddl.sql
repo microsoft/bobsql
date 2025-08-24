82f24f61ec70c5ac0edc43b4bad27de5fd76713d
Bob Ward <bobward@microsoft.com>
2025-08-23 19:13:37 -0500

    regex examples

--- sql2025book/ch5 - Developer/regex/regex_ddl.sql
+++ sql2025book/ch5 - Developer/regex/regex_ddl.sql
@@ -16,11 +16,10 @@ DROP TABLE IF EXISTS EMPLOYEES;
 GO
 CREATE TABLE EMPLOYEES (  
     ID INT IDENTITY PRIMARY KEY CLUSTERED,  
-    [Name] VARCHAR(150),  
-    Email VARCHAR(320)  
+    [Name] VARCHAR(150),
+    Email VARCHAR(320)
     CHECK (REGEXP_LIKE(Email, '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$')),  
     PhoneNumber NVARCHAR(20)  
-    CHECK (REGEXP_LIKE(PhoneNumber,'^\+\d{1,3}[ -]?(?:\([2-9]\d{2}\)[ -]?\d{3}-\d{4}|[2-9]\d{2}[ -]?\d{3}-\d{4})$')
-    )
+    CHECK (REGEXP_LIKE(PhoneNumber, '^(?:\+\d{1,3}[ -]?)?(?:\([2-9]\d{2}\)[ -]?\d{3}-\d{4}|[2-9]\d{2}[ -]?\d{3}-\d{4})$') )
 );  
 GO
